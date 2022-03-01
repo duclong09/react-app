8205e090451054f5593955661d87b682da6f2df6
Đức Long <duclongsew@gmail.com>
2022-03-01 17:18:05 +0700

    Hook Two-way Two-way binding Check radio Check checkbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Binding from './Binding';
 import Gift from './Gift';
 
 
@@ -8,6 +9,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
     <Gift/>
+    <Binding/>
   </React.StrictMode>,
   document.getElementById('root')
 );
