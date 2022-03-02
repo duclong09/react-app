0b37de5d373bcf2ed2eb605774a12d8cacce3214
Đức Long <duclongsew@gmail.com>
2022-03-02 16:12:53 +0700

    Hook Effect Dom Element -Add Scroll -Add inner width

--- src/Content.js
+++ src/Content.js
@@ -4,11 +4,16 @@ import { useEffect, useState } from "react";
 const tabs = ["todos", "comments", "albums","photos","users"]
 
 export const Content = () => {
-   
+   //cua so trinh duyet:
+   const [width, setWidth] = useState(window.innerWidth)
+
+
+
   const [title, setTitle] = useState("");
   const [todos, setTodos] = useState([]);
   const [type,setType] = useState('todos')
-  console.log(type)
+  const [showGoToTop, setShowGoToTop] = useState(false)
+  //console.log(type)
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${type}`)
@@ -17,6 +22,42 @@ export const Content = () => {
         setTodos(todos);
       });
   }, [type]);
+
+
+  //Dom event listener
+  useEffect(()=> {
+    const handleScroll = () => {
+        if(window.scrollY >=200){
+            setShowGoToTop(true)
+            // console.log('set state')
+        }else{
+            setShowGoToTop(false)
+        }
+    }
+    window.addEventListener('scroll', handleScroll)
+    console.log('addEventListener')
+    //clean function
+
+    return ()=>{
+        window.removeEventListener('scroll', handleScroll)
+        console.log('removeEventListener')
+    }
+  },[])
+
+
+  useEffect(() => {
+      const handleRisize = ()=>{
+        setWidth(window.innerWidth)
+      }
+    window.addEventListener('resize', handleRisize)
+
+    //cleanup func
+    return ()=>{
+        window.removeEventListener('resize', handleRisize)
+    }
+  },[])
+
+
 
   return (
     <div>
@@ -39,6 +80,22 @@ export const Content = () => {
           <li key={todo.id}>{todo.title || todo.name}</li>
         ))}
       </ul>
+      {showGoToTop && (
+          <button style={{
+              position: 'fixed',
+              right: 20,
+              bottom: 20,
+          }}>
+              Go to Top
+          </button>
+      )}
+
+      <div>
+          <h1>{width}</h1>
+      </div>
     </div>
+
+
+    
   );
 };
